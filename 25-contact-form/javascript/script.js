f06f9c480647358d3b9afbd3ab7acd8786c8f89b
Titus210 <tituskiplagat210@gmail.com>
2024-10-08 18:01:15 +0300

    initializing all input elements and getting them from the dom

---
+++ 25-contact-form/javascript/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const firstNameInput = document.getElementById("first-name");
+    const lastNameInput = document.getElementById("last-name");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+    const consentCheckbox = document.getElementById("consent");
+    const queryRadios = document.querySelectorAll("input[name='query-type']");
+    const modalContainer = document.querySelector(".modal-container");
