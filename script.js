a6b8e534e9d981e259acc7b06f1b18d24bc04c7f
Edasgh <edas25564@gmail.com>
2024-10-24 14:17:24 +0530

    "to-do-list added"

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ const projects = [
   { name: "22-Weather-App", tags: ["HTML", "CSS", "JavaScript", "API"] },
   { name: "23-joke-generator", tags: ["HTML", "CSS", "JavaScript", "API"] },
   { name: "24-login-signup", tags: ["HTML", "CSS", "JavaScript", "API"] },
+  { name: "26-guess-the-number-game", tags: ["HTML", "CSS", "JavaScript"] },
   { name: "26-shoe-website", tags: ["HTML", "CSS", "JavaScript"] },
   { name: "27-swiggy-clone", tags: ["HTML", "CSS", "JavaScript"] },
-  { name: "28-Zomato-clone", tags: ["HTML", "CSS", "JavaScript"] }
-
-
+  { name: "28-Zomato-clone", tags: ["HTML", "CSS", "JavaScript"] },
+  { name: "29-to-do-list", tags: ["HTML", "CSS", "JavaScript"] },
 ];
     
 
