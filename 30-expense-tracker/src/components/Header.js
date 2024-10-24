3f7b36fb11e999513185076266a328ccce002b20
Riya Sharma <riyaprol9@gmail.com>
2024-10-24 10:45:32 +0530

    Added new projects and updated script.js

---
+++ 30-expense-tracker/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header=()=> {
+    return (
+        <div>
+            <h1>
+        Expense Tracker
+        </h1>
+    </div>
+    )
+}
---
+++ 30-expense-tracker/src/components/IncomeExpenses.js
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+// //Money formatter function
+// function moneyFormatter(num) {
+//   let p = num.toFixed(2).split('.');
+//   return (
+//     '$ ' +
+//     p[0]
+//       .split('')
+//       .reverse()
+//       .reduce(function (acc, num, i, orig) {
+//         return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+//       }, '') +
+//     '.' +
+//     p[1]
+//   );
+// }
+
+export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  );
+
+  return (
+    <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+  <p className="money plus">{income}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+  <p className="money minus">{expense}</p>
+        </div>
+      </div>
+  )
+}
